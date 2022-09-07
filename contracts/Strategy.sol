fcba25a161b333e2a5aac26183782a65eaba0701
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-09-07 13:07:39 +0300

    fix

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.10;
 
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@aave/core-v3/contracts/protocol/pool/L2Pool.sol";
 
 contract Strategy {
     address UniswapV2Router02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-    address USDCAddress = 0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C;
+    address USDCAddress = 0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43;
     mapping(address => uint256) public userPositions;
 
     event Deposit(uint256 amount);
+    event Log(bool);
+    event Log2(uint256);
 
     function deposit() public payable {
         address[] memory path = new address[](2);
@@ -17,7 +20,7 @@ contract Strategy {
         path[1] = USDCAddress;
 
         uint256[] memory amounts = IUniswapV2Router02(UniswapV2Router02)
-            .swapExactETHForTokens{value: msg.value}(
+            .swapExactETHForTokens{value: (msg.value * 90) / 100}(
             1 wei,
             path,
             address(this),
@@ -25,6 +28,19 @@ contract Strategy {
         );
 
         userPositions[msg.sender] = amounts[1];
+
+        bool result = IERC20(USDCAddress).approve(
+            0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6,
+            amounts[1]
+        );
+
+        L2Pool(0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6).supply(
+            USDCAddress,
+            amounts[1],
+            address(this),
+            0
+        );
+
         emit Deposit(amounts[1]);
     }
 
